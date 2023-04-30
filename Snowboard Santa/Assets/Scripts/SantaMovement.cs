2a6aaf19c23078fee117b9bb9f2f1aa8f8e4cb77
MarcusOtter <github@otterstrom.dev>
2023-04-30 07:42:59 +0200

    Coyote and jump buffer

--- Snowboard Santa/Assets/Scripts/SantaMovement.cs
+++ Snowboard Santa/Assets/Scripts/SantaMovement.cs
@@ -14,12 +14,17 @@ public class SantaMovement : MonoBehaviour
     [SerializeField] private float fallMultiplier = 2f;
     [SerializeField] private float lowJumpMultiplier = 4f;
     [SerializeField] private float rotationSpeed = 500f;
+    [SerializeField] private float coyoteTime = 0.25f;
+    [SerializeField] private float jumpBuffer = 0.25f;
 
     [Header("References")]
     [SerializeField] private BoxCollider2D groundedCollider;
     [SerializeField] private LayerMask jumpableLayers;
 
     private Rigidbody2D _rigidbody;
+
+    private float _coyoteTimer;
+    private float _jumpBufferTimer;
 
     private void Awake()
     {
@@ -31,10 +36,35 @@ public class SantaMovement : MonoBehaviour
         _rigidbody.velocity = new Vector2(speed, _rigidbody.velocity.y);
         IsGrounded = CheckGrounded();
         
-        if (Input.GetButtonDown("Jump") && IsGrounded)
+        if (IsGrounded)
         {
+            _coyoteTimer = coyoteTime;
+        }
+        else
+        {
+            _coyoteTimer -= Time.deltaTime;
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            _jumpBufferTimer = jumpBuffer;
+        }
+        else
+        {
+            _jumpBufferTimer -= Time.deltaTime;
+        }
+        
+        if (_jumpBufferTimer > 0 && _coyoteTimer > 0)
+        {
+            _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, 0);
             _rigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
             OnJump?.Invoke();
+            _jumpBufferTimer = 0;
+        }
+
+        if (Input.GetButtonUp("Jump"))
+        {
+            _coyoteTimer = 0;
         }
 
         if (_rigidbody.velocity.y < 0)

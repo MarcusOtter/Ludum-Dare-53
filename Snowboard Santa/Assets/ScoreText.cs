ee4de825d3ceea08f19e70ef16edea258065a2f3
Dan Burt <gromitzy@gmail.com>
2023-05-02 02:37:00 +0200

    wip

---
+++ Snowboard Santa/Assets/ScoreText.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class ScoreText : MonoBehaviour
+{
+    private TextMeshProUGUI text;
+    public string scoreMessage;
+    public float rotationDelta, rotationDeltaMax;
+    void Start()
+    {
+        text = gameObject.AddComponent<TextMeshProUGUI>();
+        text.text = scoreMessage;
+        text.color = new Color(1f, 1f, 1f, 0f);
+        transform.localScale = Vector3.one * 0.6f;
+        rotationDelta = Random.Range(-rotationDeltaMax, rotationDeltaMax);
+        transform.position += new Vector3(100f, 100f, 0);
+        transform.RotateAround();
+    }
+
+    void Update()
+    {
+        text.color = Color.Lerp(text.color, Color.white, 6f * Time.deltaTime);
+        transform.localScale = Vector3.Lerp(transform.localScale, Vector3.one, 6f * Time.deltaTime);
+        transform.RotateAround(Vector3.forward, rotationDelta);
+    }
+}
